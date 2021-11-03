e07c39c1593940333c6941891dc66ea726a14dae
sarslana <serdalarslan0334@gmail.com>
2021-11-03 18:49:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#p_1. Create a function that takes two numbers as arguments and return their sum.
+
+#Examples
+#addition(3, 2) ➞ 5
+#addition(-3, -6) ➞ - 9
+#addition(7, 3) ➞ 10
+print("Welcome to addition calculator")
+first_number=float(input('What is your first number? '))
+second_number=float(input('What is your second number? '))
+sum= float(first_number+second_number)
+print(f'Addition of your number is {sum}')
+print('Thank you for using Addition Calculator')
