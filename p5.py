2e64bbbd9ebd91ba72f19ac5e306ed44f3048361
sarslana <serdalarslan0334@gmail.com>
2021-11-13 11:32:40 -0500

    merhabalar, dun fonksiyonu nerde demistiniz, biraz calsiinca oluyormus, fonksiyon ekledim, tesekkurler

--- p5.py
+++ p5.py
@@ -1,7 +1,16 @@
 #p_5. Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
 
-print('Possible longest side of your choice triangle')
+#print('Possible longest side of your choice triangle')
+
 side1=int(input('What is the first side of your triangle in cm? '))
 side2=int(input('What is the second side of your triangle in cm? '))
-maximum = int(side1+side2-1)
-print(f'The maximum length of third side of your choice triangle is {maximum} cm')
+
+
+def maximum(side1,side2):
+    return side1+side2-1
+maximum(side1,side2)
+n=maximum(side1,side2)
+
+print(f"Maximum value of the third side is {n}")
+
+
