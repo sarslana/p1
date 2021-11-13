05454bff12d84dd0d09ff5c11f27940c2689f809
sarslana <serdalarslan0334@gmail.com>
2021-11-13 11:37:44 -0500

    buna da fonksiyon ekledim

--- p4.py
+++ p4.py
@@ -1,7 +1,12 @@
 #p_4. Write a function that takes the base and height of a triangle and return its area.
 
 print('Area of triangle calculator')
+
 base=round(float(input('What is the base of your triangle in cm? ')),2)
 height=round(float(input('What is the height of your triangle in cm? ')), 2)
-area = round(float(base*height/2), 2)
-print(f'Area of your choice triangle is {area}')
+
+def area(base, height):
+    return round(float(base*height/2), 2)
+area(base, height)
+n = area(base, height)
+print(f'Area of your choice triangle is {n}')
