6bd995af860aeb8444a0a428629004d2459e4675
sarslana <serdalarslan0334@gmail.com>
2021-11-17 21:52:11 -0500

    10 u yaptim

---
+++ p10.py
@@ -0,0 +1,11 @@
+#p_10. Create a function that takes a number num and returns its length.
+
+#Examples
+#number_length(10) ➞ 2
+#number_length(5000) ➞ 4
+#number_length(0) ➞ 1
+
+num = str(input("What is your number?  "))
+def number_len():
+    return print(len(num))
+number_len()
